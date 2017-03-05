44e8b994d260e85b8df25edbec3cd185a54c1a40
rasna shakya <rasnashakya@gmail.com>
2017-03-06 03:55:22 +0545

    db migrated

--- db/migrate/20170305220701_add_column_to_post.rb
+++ db/migrate/20170305220701_add_column_to_post.rb
@@ -1,5 +1,5 @@
 class AddColumnToPost < ActiveRecord::Migration[5.0]
   def change
-    add_column :posts, :video_processing, :boolean, , null: false, default: false
+    add_column :posts, :video_processing, :boolean , null: false, default: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170305213728) do
+ActiveRecord::Schema.define(version: 20170305220701) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "title"
     t.text     "description"
     t.string   "video"
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
-    t.boolean  "sucess",      default: false, null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+    t.boolean  "sucess",           default: false, null: false
+    t.boolean  "video_processing", default: false, null: false
   end
 
   create_table "users", force: :cascade do |t|
