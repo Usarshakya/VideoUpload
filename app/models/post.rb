3ea63dcbca1870f1ddf86c5505474417850bf406
rasna shakya <rasnashakya@gmail.com>
2017-03-05 23:14:33 +0545

    added validation to post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
 	mount_uploader :video, VideoUploader
+
+	validates :title, presence: true
+  validates :description, presence: true
+  validates :video, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,10 +16,8 @@ ActiveRecord::Schema.define(version: 20170305143727) do
     t.text     "title"
     t.text     "description"
     t.string   "video"
-    t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
