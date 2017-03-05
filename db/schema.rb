4c7587871b7731d4303f694d1662f37fe44a98a9
rasna shakya <rasnashakya@gmail.com>
2017-03-06 03:26:16 +0545

    created table

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170305143727) do
+ActiveRecord::Schema.define(version: 20170305213728) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "title"
     t.text     "description"
     t.string   "video"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.boolean  "sucess",      default: false, null: false
   end
 
   create_table "users", force: :cascade do |t|
